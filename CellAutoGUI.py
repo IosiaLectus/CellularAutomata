a2ddd6ccbee903fede1e9b76fb884ee48e050181
Josiah Couch <josiah.couch@gmail.com>
2017-05-05 08:02:05 -0500

    Implemented main menu

--- CellAutoGUI.py
+++ CellAutoGUI.py
@@ -12,23 +12,24 @@ GREEN = (0,255,0)
 BLUE = (0,0,255)
 
 clock = pygame.time.Clock()
-pygame.init()
-button_font = pygame.font.SysFont('FreeSans.otf',15)
 
 WIDTH = 960
 HEIGHT = 540
 SIZE = 120
-RULE = 30
+RULE = 82552976365228637171366736826016
+STATES = 5
 
 COLORS = [BLACK,WHITE,RED,GREEN,BLUE]
 
 FPS = 5
 
 #Run a 1-d cellular automaton with 'size' cells and rule 'rule'.
-def run_1DAutomata(screen, size, rule):
+def run_1DAutomata(screen, size, rule, n_states):
+
+    button_font = pygame.font.SysFont('FreeSans.otf',25)
 
     # Create the cellular automaton object
-    Aut = Automata1D(size,rule)
+    Aut = Automata1D(size,rule, n_states)
     # Given the screen size and the size of the automaton, decide the size of each cell in pixels.
     dt = WIDTH/Aut.size
     # Create a live cell in the center
@@ -40,13 +41,17 @@ def run_1DAutomata(screen, size, rule):
     grid = []
 
     # Make some buttons
-    start_stop_button = button_font.render("Start/Stop",True,BLACK,LIGHT_GRAY)
+    start_stop_button = button_font.render("Start/Stop",True,WHITE,BLACK)
     start_stop_button_rect = start_stop_button.get_rect()
     start_stop_button_rect.topleft = (0.15 * WIDTH, 0.88 * HEIGHT)
 
-    clear_button = button_font.render("Clear",True,BLACK,LIGHT_GRAY)
+    clear_button = button_font.render("Clear",True,WHITE,BLACK)
     clear_button_rect = clear_button.get_rect()
     clear_button_rect.topleft = (0.35 * WIDTH, 0.88 * HEIGHT)
+
+    menu_button = button_font.render("Menu",True,WHITE,BLACK)
+    menu_button_rect = menu_button.get_rect()
+    menu_button_rect.topleft = (0.55 * WIDTH, 0.88 * HEIGHT)
     
     # change the caption
     caption = '1-Dimensional Cellular Automaton: Rule %u' %(rule)
@@ -68,7 +73,9 @@ def run_1DAutomata(screen, size, rule):
                     Aut = Automata1D(size,rule)
                     Aut.populate(int(size/2))
                     grid = []
-                    screen.fill(WHITE)
+                    screen.fill(LIGHT_GRAY)
+                elif event.key == K_m:
+                    return
             elif event.type == MOUSEBUTTONUP:
                 if start_stop_button_rect.collidepoint(event.pos):
                     running = not running
@@ -76,7 +83,9 @@ def run_1DAutomata(screen, size, rule):
                     Aut = Automata1D(size,rule)
                     Aut.populate(int(size/2))
                     grid = []
-                    screen.fill(WHITE)
+                    screen.fill(LIGHT_GRAY)
+                elif menu_button_rect.collidepoint(event.pos):
+                    return
         if running:
             # Get the list of current cells, and append them at the bottom of our grid
             grid.append(list(Aut.present))
@@ -84,7 +93,7 @@ def run_1DAutomata(screen, size, rule):
             if len(grid)> display_height/dt:
                 grid = list(grid[1:])
             # Draw the cells to the screen
-            screen.fill(WHITE)
+            screen.fill(LIGHT_GRAY)
             for i in range(0,len(grid)):
                 for j in range(0,Aut.size):
                     pygame.draw.rect(screen,COLORS[grid[i][j]],(j*dt,i*dt,dt,dt),0)
@@ -95,6 +104,7 @@ def run_1DAutomata(screen, size, rule):
         #Draw the buttons
         screen.blit(start_stop_button, start_stop_button_rect)
         screen.blit(clear_button, clear_button_rect)
+        screen.blit(menu_button, menu_button_rect)
 
 	# Update display and increment the clock
         pygame.display.update()
@@ -102,8 +112,39 @@ def run_1DAutomata(screen, size, rule):
 
 #Display a menu (not yet implemented).
 def display_menu(screen):
-    # For now, just run, just run a 1-d cellular automaton
-    run_1DAutomata(screen, SIZE, RULE)
+
+    button_font = pygame.font.SysFont('FreeSans.otf',45)
+
+    # Make some buttons
+    cell_at_oned_button = button_font.render("Start 1-d cellular automaton",True,WHITE,BLACK)
+    cell_at_oned_button_rect = cell_at_oned_button.get_rect()
+    w_button = (WIDTH - cell_at_oned_button_rect.width)/2
+    cell_at_oned_button_rect.topleft = (w_button, 0.1 * HEIGHT)
+
+    # Setup the loop
+    should_continue = True  
+    while should_continue:
+        # Get events and respond accordingly
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYUP:
+                if event.key == K_a:
+                    # Run a 1-d cellular automaton
+                    run_1DAutomata(screen, SIZE, RULE, STATES)
+
+            elif event.type == MOUSEBUTTONUP:
+                if cell_at_oned_button_rect.collidepoint(event.pos):
+                    # Run a 1-d cellular automaton
+                    run_1DAutomata(screen, SIZE, RULE, STATES)
+        #Fill screen, draw button
+        screen.fill(LIGHT_GRAY)
+        screen.blit(cell_at_oned_button, cell_at_oned_button_rect)
+
+	# Update display and increment the clock
+        pygame.display.update()
+        clock.tick(FPS)
 
 #Actually do stuff
 def main():
--- CellularAutomata.py
+++ CellularAutomata.py
@@ -3,16 +3,15 @@ import random
 
 class Automata1D:
 
-    # Number of states of a single cell
-    CELL_STATES = 2
-    # Size of a neighborhood
-    NBHDSIZE = 3
-    # The number of distinct states a neighborhood can be in.
-    NUM_STATES = CELL_STATES**NBHDSIZE
-    # The number of possible rules:
-    NUM_RULES = NUM_STATES**NUM_STATES
-    
-    def __init__(self,s,r):
+    def __init__(self,s,r,cs=2):
+        # Number of states of a single cell
+        self.cell_states = cs
+        # Size of a neighborhood
+        self.nbhd_size = 3
+        # The number of distinct states a neighborhood can be in.
+        self.num_states = self.cell_states**self.nbhd_size
+        # The number of possible rules:
+        self.max_rule = self.cell_states**self.num_states
         #lists for storing the state of the system at three differnt time steps.
         self.past = [0 for i in range(0,s)]
         self.present = list(self.past)
@@ -20,13 +19,13 @@ class Automata1D:
         # The number of cells in our automaton
         self.size = s
         # Rule stored a an integer. This will get parsed into a lookup table
-	self.rule = r%self.NUM_RULES
+	self.rule = r%self.max_rule
         # The rule transformed into a lookup table
-        self.ruleArray = self.parseRule(r)
+        self.ruleArray = self.parseRule(self.rule)
         
     # Change the rule
     def setRule(self,r):
-        self.rule = r%self.NUM_RULES
+        self.rule = r%self.max_rule
         self.ruleArray = self.parseRule(r)
     
     # Get the neighborhood of a cell as a list. Note that cell neighborhoods wrap (the first and last cells are neighbors).
@@ -38,19 +37,19 @@ class Automata1D:
         nbrs = self.Neighbors(n)
         state = 0
         for i in range(0,len(nbrs)):
-            state += (self.CELL_STATES**i)*self.present[nbrs[i]]
+            state += (self.cell_states**i)*self.present[nbrs[i]]
         return self.ruleArray[state]
 
     # Create a lookup table based on rule
     def parseRule(self, x):
-        y = x
-        ret = [0 for i in range(0, self.NUM_STATES)]
+        y = x % self.max_rule
+        ret = [0 for i in range(0, self.num_states)]
         i = 0
         while(y>0):
-            ret[i] = y%self.CELL_STATES
+            ret[i] = y%self.cell_states
             i = i + 1
-            y = y - y%self.CELL_STATES
-            y = y/self.CELL_STATES
+            y = y - y%self.cell_states
+            y = y/self.cell_states
         return ret
     
     # Update the automaton by one time step.
